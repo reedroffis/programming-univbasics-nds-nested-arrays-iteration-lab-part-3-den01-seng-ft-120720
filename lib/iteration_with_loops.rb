9437e173aabb20138834c0dcf043639379e63ae6
Reed Roffis <reedroffis18@gmail.com>
2020-12-02 18:48:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,14 @@ def join_nested_strings(src)
   while row_index < src.count do 
     element_index = 0 
     while element_index < src[row_index].count do
+      element = src(String) 
+      if element_index.class == String 
+        output + "element_index"
+      end
+      element_index += 1
     end
-    element_index += 1
+    row_index += 1 
   end
-  
 end
 
 # Hey future Roff. make sure to use .class to see if the elemnt is a string or not. That's probably where you should start.
